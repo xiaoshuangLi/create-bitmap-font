8a38b0bed34e433efb0d80696c6e7c22aab9e262
xiaoshuang.li <qq1157502759@gmail.com>
2018-09-14 17:13:46 +0800

    upgrade Babel => 7.0 2.0

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -55,9 +55,6 @@ module.exports = {
         use: [
           {
             loader: 'babel-loader',
-            options: {
-              presets: ['es2015', 'react'],
-            },
           },
           {
             loader: 'svg-react-loader',
